ab6730026fe5a4ad34b87425761857f6f054d5af
eugenie <eugenie.margulis@gmail.com>
2019-06-26 18:26:27 +0200

    connexion et création de compte

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import FirebaseProvider from './Firebase/FirebaseProvider';
 import { HashRouter, Switch, Route } from 'react-router-dom';
-import Dashboard from './components/dashboard';
+import Dashboard from './components/Dashboard';
+import Welcome from './components/connexion/Welcome';
+import Connect from './components/connexion/Connect';
+import Signin from './components/connexion/Signin';
+import PasswordForget from './components/connexion/PasswordForget';
 import './App.css';
 
 
@@ -10,7 +14,11 @@ const App = () => (
     <FirebaseProvider>
       <HashRouter>
         <Switch>
-          <Route exact path="/" component={Dashboard} />
+        <Route exact path="/" component={Welcome} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/signin" component={Signin} />
+        <Route path="/connect" component={Connect} />
+        <Route path="/reset" component={PasswordForget} />
         </Switch>
       </HashRouter>
     </FirebaseProvider>
