b303b2450912f082e790fced37f57888ca0de5ae
Lau <laurent.henriques@gmail.com>
2019-06-28 10:37:45 +0200

    fix

--- src/components/spendcredits/QrCode.js
+++ src/components/spendcredits/QrCode.js
@@ -3,7 +3,7 @@ let QRCode = require('qrcode.react');
 
 const QrCode = (props) => {
   const value = parseInt(props.location.state.value.prix.split('c'))
-  return ( <div> <QRCode style={{width: '50%', height: '50%', marginTop: '89px'}} value={`https://unicity.netlify.com/sendCredits/${value}`} />
+  return ( <div> <QRCode style={{width: '50%', height: '50%', marginTop: '89px'}} value={`https://unicity.netlify.com/SendCredits/${value}`} />
   <div>  
     <h1>  
   Scannez le code pour obtenir votre {props.location.state.value.nom} !  <div> Vous serez débité de {value} ! </div> </h1></div></div> );
