4665c31eb97efc06ae5414ed506fecc5703abfc8
RomainCR <romain.crey@gmail.com>
2019-06-28 08:45:45 +0200

    fix annonce

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,57 +1,59 @@
-import React, { useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import clsx from "clsx";
-import Card from "@material-ui/core/Card";
+import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
 import { Link } from 'react-router-dom';
-import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
-import CardActions from "@material-ui/core/CardActions";
-import Collapse from "@material-ui/core/Collapse";
-import Avatar from "@material-ui/core/Avatar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import { red } from "@material-ui/core/colors";
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
 import AccountBox from '@material-ui/icons/AccountBox';
-import ShareIcon from "@material-ui/icons/Share";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import * as firebase from 'firebase';
-import { flexbox, border } from "@material-ui/system";
+import { flexbox, border } from '@material-ui/system';
 
 const picto = {
-  "Nature et jardinage": 'https://i.ibb.co/gRxQRNC/nature.png',
-  "Ecologie et nettoyage": 'https://i.ibb.co/q1KScvP/ecologie.png',
-  "Loisir et divertissement": 'https://i.ibb.co/p2gd20D/loisir.png',
-  "Aide à la personne": 'https://i.ibb.co/9YqWPBC/brico.png',
-  "Réparation et bricolage": 'https://i.ibb.co/9YqWPBC/brico.png',
-  "Education": 'https://i.ibb.co/NWmx5KC/ducation.png'
-}
-
+  'Nature et jardinage': 'https://i.ibb.co/gRxQRNC/nature.png',
+  'Ecologie et nettoyage': 'https://i.ibb.co/q1KScvP/ecologie.png',
+  'Loisir et divertissement': 'https://i.ibb.co/p2gd20D/loisir.png',
+  'Aide à la personne': 'https://i.ibb.co/9YqWPBC/brico.png',
+  'Réparation et bricolage': 'https://i.ibb.co/9YqWPBC/brico.png',
+  Education: 'https://i.ibb.co/NWmx5KC/ducation.png',
+};
 
 
 const useStyles = makeStyles(theme => ({
   card: {
     maxWidth: 590,
-    //height: 300,
+    // height: 300,
   },
   media: {
     height: 0,
-    paddingTop: "56.25%" // 16:9
+    paddingTop: '56.25%', // 16:9
   },
   expand: {
-    transform: "rotate(0deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest
-    })
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
   },
   expandOpen: {
-    transform: "rotate(180deg)"
+    transform: 'rotate(180deg)',
   },
   avatar: {
-    backgroundColor: red[500]
-  }
+    backgroundColor: red[500],
+  },
+  title: {
+    scale: 'transform(1.2)',
+  },
 }));
 
 export default function UserProfile(props) {
@@ -62,52 +64,51 @@ export default function UserProfile(props) {
     setExpanded(!expanded);
   }
 
-  const { user } = props
+  const { user } = props;
   return (
     <div>
       <Card className={classes.card}>
         <CardHeader
-          avatar={
+          avatar={(
             <Avatar aria-label="Recipe" className={classes.avatar}>
               {user && user.data.name[0]}
             </Avatar>
-          }
-          action={
-            <IconButton aria-label="Settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
+          )}
+          action={(
+            <CardActions disableSpacing>
+              <IconButton aria-label="Add to favorites">
+                <Link to={`/publicprofile/${user.id}`}><AccountBox /></Link>
+              </IconButton>
+            </CardActions>
+          )}
           title={user && user.data.name}
           subheader={user && user.data.adress}
         />
         <CardContent>
           <div style={{
-            display: "flex"
-          }}>
+            display: 'flex',
+          }}
+          >
             <Typography variant="body2" color="textSecondary">
-              {user && user.data.competences.map(comp =>
-                  <img src={picto[comp]}
-                    key={Math.floor(Math.random() * 5000)}
-                    style={{
-                      backgroundColor: "347B98",
-                      width: '20%',
-                      borderRadius: "200px",
-                      margin: "5%"
-                    }} />
-                )}
+              {user && user.data.competences.map(comp => (
+                <img
+                  alt={comp}
+                  src={picto[comp]}
+                  key={Math.floor(Math.random() * 5000)}
+                  style={{
+                    backgroundColor: '#347B98',
+                    width: '15%',
+                    borderRadius: '200px',
+                    margin: '4%',
+                  }}
+                />
+              ))}
             </Typography>
           </div>
         </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label="Add to favorites">
-            <Link to={`/publicprofile/${user.id}`}><AccountBox /></Link>
-          </IconButton>
-          <IconButton aria-label="Share">
-            <ShareIcon />
-          </IconButton>
-          <IconButton
+        {/* <IconButton
             className={clsx(classes.expand, {
-              [classes.expandOpen]: expanded
+              [classes.expandOpen]: expanded,
             })}
             onClick={handleExpandClick}
             aria-expanded={expanded}
@@ -115,15 +116,15 @@ export default function UserProfile(props) {
           >
             <ExpandMoreIcon />
           </IconButton>
-        </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
+
+         <Collapse in={expanded} timeout="auto" unmountOnExit>
           <CardContent>
             <Typography paragraph>Bio :</Typography>
             <Typography paragraph>
               {user && user.data.bio}
             </Typography>
           </CardContent>
-        </Collapse>
+          </Collapse> */}
       </Card>
     </div>
   );
--- src/components/annonces/SeeAnnounce.js
+++ src/components/annonces/SeeAnnounce.js
@@ -1,20 +1,26 @@
-import React, { Component } from "react";
-import withFirebaseContext from "../../Firebase/withFirebaseContext";
-import { withRouter } from "react-router-dom";
-import Button from "@material-ui/core/Button";
-import Avatar from "../Avatar";
-import Coins from "../Coins";
-import TextField from "@material-ui/core/TextField";
-import UntouchableCard from "./UntouchableCard";
-import * as firebase from "firebase";
-import ApplyList from "../apply/applyList";
-import ArrowBack from "@material-ui/icons/ArrowBack";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import * as firebase from 'firebase';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+import {
+  TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton,
+} from 'react-twitter-embed';
+import withFirebaseContext from '../../Firebase/withFirebaseContext';
+import Avatar from '../Avatar';
+import Coins from '../Coins';
+import UntouchableCard from './UntouchableCard';
+import ApplyList from '../apply/applyList';
 
 
 class SeeAnnounce extends Component {
   constructor(props) {
     super(props);
-    this.state = { message: "" };
+    this.state = {
+      message: '',
+      buttonPostuler: 1,
+    };
   }
 
   componentDidMount() {
@@ -25,20 +31,20 @@ class SeeAnnounce extends Component {
   getAnnounceFromDB = () => {
     const { match } = this.props;
     const id = match.params.annonceid;
-    let annonce = [];
+    const annonce = [];
     const { firestore } = this.props;
     firestore
-      .collection("annonces")
+      .collection('annonces')
       .doc(id)
       .get()
-      .then(document => {
+      .then((document) => {
         const doc = document.data();
 
         annonce.push(doc);
 
         this.setState({
           annonce,
-          id
+          id,
         });
       });
   };
@@ -47,24 +53,25 @@ class SeeAnnounce extends Component {
     const user = [];
     firebase
       .firestore()
-      .collection("usersinfo")
-      .doc(`${localStorage.getItem("userId")}`)
+      .collection('usersinfo')
+      .doc(`${localStorage.getItem('userId')}`)
 
       .get()
-      .then(doc => {
+      .then((doc) => {
         user.push(doc.data());
       });
 
     this.setState({
-      user
+      user,
     });
   };
+
   updateToInProgress = () => {
     const { firestore } = this.props;
-    const { id } = this.state
-    const annonceRef = firestore.collection("annonces").doc(id);
+    const { id } = this.state;
+    const annonceRef = firestore.collection('annonces').doc(id);
     annonceRef.update({
-      inProgress: true
+      inProgress: true,
     });
   };
 
@@ -73,110 +80,130 @@ class SeeAnnounce extends Component {
 
     if (
       annonce[0].postulants
-        .map(item => item.id === localStorage.getItem("userId"))
-        .includes(false) ||
-      (annonce[0].postulants.map(
-        item => item.id === localStorage.getItem("userId")
-      ).length === 0 &&
-        !apply)
+        .map(item => item.id === localStorage.getItem('userId'))
+        .includes(false)
+      || (annonce[0].postulants.map(
+        item => item.id === localStorage.getItem('userId'),
+      ).length === 0
+        && !apply)
     ) {
       const { match } = this.props;
       const id = match.params.annonceid;
       firebase
         .firestore()
-        .collection("annonces")
+        .collection('annonces')
         .doc(id)
         .update({
           postulants: firebase.firestore.FieldValue.arrayUnion({
-            id: localStorage.getItem("userId"),
-            date: Date(Date.now()).toString()
-          })
+            id: localStorage.getItem('userId'),
+            date: Date(Date.now()).toString(),
+          }),
         });
       this.setState({
-        message: "Votre candidature a bien été prise en compte.",
-        apply: true
+        buttonPostuler: 0,
+        message: 'Votre candidature a bien été prise en compte.',
+        apply: true,
       });
     } else {
-      const date = annonce[0].postulants.filter(annonce =>
-        annonce.id.includes(localStorage.getItem("userId"))
-      );
+      const date = annonce[0].postulants.filter(annonce => annonce.id.includes(localStorage.getItem('userId')));
 
       this.setState({
         message: `Vous avez déjà postulé à cette annonce le ${
           date[0].date
-          } , votre candidature sera traitée dans les plus brefs délais.`
+        } , votre candidature sera traitée dans les plus brefs délais.`,
       });
     }
   };
 
   render() {
-    const { annonce, message, id, user } = this.state;
+    const {
+      annonce, message, id, user, buttonPostuler,
+    } = this.state;
+    console.log(buttonPostuler);
 
     return (
       <div>
-        {" "}
+        {' '}
         <ArrowBack
-          style={{ position: "fixed", left: "10px", top: "10px" }}
+          style={{ position: 'fixed', left: '10px', top: '10px' }}
           onClick={() => {
             const { history } = this.props;
             this.setState({
               showAll: false,
-              choice: undefined
+              choice: undefined,
             });
-            history.push("/dashboard");
+            history.push('/dashboard');
           }}
         />
         {annonce ? (
           <>
-            {" "}
-            <h1 style={{ marginTop: "20px" }}>{annonce[0].titre}</h1>{" "}
+            {' '}
+            <h1 style={{ marginTop: '20px' }}>{annonce[0].titre}</h1>
+            {' '}
             <UntouchableCard annonce={annonce[0]} />
-            <p style={{ marginTop: "20px" }}>
-              {annonce[0].points}{" "}
+            <p style={{ marginTop: '20px' }}>
+              {annonce[0].points}
+              {' '}
               <img
                 className="money"
                 style={{
-                  width: "5%",
-                  height: "5%",
-                  marginRight: "5%",
-                  marginTop: "4%",
-                  marginLeft: "1%"
+                  width: '5%',
+                  height: '5%',
+                  marginRight: '5%',
+                  marginTop: '4%',
+                  marginLeft: '1%',
                 }}
                 src="https://i.ibb.co/SnyPLSx/money.png"
                 alt="money"
-              />{" "}
-            </p>{" "}
-            <div style={{ marginTop: "50px" }}>
-              {" "}
+              />
+              {' '}
+            </p>
+            {' '}
+            <div style={{ marginTop: '50px' }}>
+              {' '}
               <div
-                style={{ padding:"9px", marginTop: "50px", width: "100%", backgroundColor: "#dad8d8", color: "black" }}
+                style={{
+                  padding: '9px', marginTop: '50px', width: '100%', backgroundColor: '#dad8d8', color: 'black',
+                }}
               >
                 {annonce[0].description}
               </div>
-              {" "}
-              <p>{message}</p>
+              {' '}
             </div>
           </>
         ) : (
-            <p> loading..</p>
-          )}{" "}
+          <p> loading..</p>
+        )}
+        {' '}
         {annonce ? (
-          user[0].isAgent === false ? <>  <h2>Vous souhaitez vous investir dans la vie de votre ville ?</h2>
-
-            <Button
-              onClick={() => {
-                this.sendMyApplication();
-              }}
-              style={{ marginTop: "50px" }}
-              variant="contained"
-            >
-              Postuler
-            </Button>
-          </>
+          user[0].isAgent === false ? (
+            <>
+              {' '}
+              <h2>Vous souhaitez vous investir dans la vie de votre ville ?</h2>
+              <Button
+                onClick={() => {
+                  this.sendMyApplication();
+                }}
+                style={{
+                  marginTop: '50px',
+                  opacity: buttonPostuler,
+                }}
+                variant="contained"
+              >
+                Postuler
+              </Button>
+              <p>{message}</p>
+            </>
+          )
             : (
               <ApplyList id={id} annonce={annonce} />
             )
         ) : null}
+        <div style={{ paddingTop: '30px' }}>
+          <TwitterHashtagButton
+            tag="uniCity"
+          />
+        </div>
       </div>
     );
   }
