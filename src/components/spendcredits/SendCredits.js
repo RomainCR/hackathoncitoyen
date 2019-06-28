f17bfefe6564675f213420ff1e449b0975c4da0a
Lau <laurent.henriques@gmail.com>
2019-06-28 11:22:33 +0200

    fix

--- src/components/spendcredits/SendCredits.js
+++ src/components/spendcredits/SendCredits.js
@@ -4,20 +4,42 @@ import { withRouter } from 'react-router-dom'
 class SendCredits extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = { user : [] }
   }
 
   componentDidMount() {
-    this.updateOver()
+    this.getUser()
   }
 
+
+  getUser = () => {
+      const user = [];
+      firebase
+        .firestore()
+        .collection("usersinfo").doc()
+  
+        .get()
+        .then(querySnapshot => {
+            console.log(querySnapshot)
+            user.push(querySnapshot.data());
+          });
+  
+          this.setState({
+            user
+          });
+      
+        console.log(user)
+  }
+  
   updateOver = () => {
+   
+ 
     const { match } = this.props
     const value = parseInt(match.params.montant)
     const id = match.params.id
     console.log(id)
     console.log(match)
-    firebase.firestore().collection(usersinfo).doc(`${id}`).update({
+    firebase.firestore().collection('usersinfo').doc(`${id}`).update({
       credits: firebase.firestore.FieldValue.increment(-value),
     });
     
